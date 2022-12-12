b65c81aa6d4a0465ffbfc6d2d463ca97bd080542
Terrence Shi <terrenceshi@gmail.com>
2022-12-12 00:35:34 -0500

    uyf

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import testImg from "./assets/test.jpg"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          hi
-        </p>
+        <div className = "banner">
+          
+          {/* put an image here later */}
+
+
+          <div className = "banner_title">
+            <h1>
+              Hi. <br></br>
+              I'm Terrence.
+            </h1>
+          </div>
+          
+
+        </div>
+
       </header>
+
     </div>
   );
 }
