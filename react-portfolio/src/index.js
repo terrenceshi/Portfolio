d37ff77e4d8bb850d4fd9396804618063b004f35
Terrence Shi <terrenceshi@gmail.com>
2023-01-08 01:01:07 -0500

    work on cs page

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ScrollToTop from './components/ScrollToTop'
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop/>
       <App />
     </BrowserRouter>
   </React.StrictMode>
